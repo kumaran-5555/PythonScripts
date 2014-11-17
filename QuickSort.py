f06e2962e79f6c3206890837d09aa8128d6d4525
serajago <kumaran_5555@outlook.com>
2014-11-17 22:51:02 +0530

    added code for quicksort

--- QuickSort.py
+++ QuickSort.py
@@ -1,16 +1,41 @@
-__author__ = 'serajago'
-l=[10,12,3,4,5,6,78,982,9,61]
+l = [ 10,12,3,4,56,7,0,8,9,11,12]
 
-def quickSort():
+class QuickSort():
+    def __init__(self, listToSort):
+        self.listToSort = listToSort
+    def __str__(self):
+        return str(self.listToSort)
 
-if ipojpoj
-    osefkosjef
-    osefjosef
-    jsefjosef
-    osefjosef
-    soefjsojfs
-    soefjsofj
-    seofjsofj
-    fojsefojsef
-    oskefjosef
-    soefksef
+    def quickSort(self, left, right):
+        if left < right:
+            pivotIdx=int(left+((right-left)/2))
+            i=left
+            j=right
+            while(i<j):
+                while (self.listToSort[i]<=self.listToSort[pivotIdx] and i<right):
+                    i=i+1
+                while(self.listToSort[j] > self.listToSort[pivotIdx]):
+                    j=j-1
+                if(i<j):
+                    temp=self.listToSort[i]
+                    self.listToSort[i]=self.listToSort[j]
+                    self.listToSort[j]=temp
+            temp=self.listToSort[pivotIdx]
+            self.listToSort[pivotIdx] = self.listToSort[j]
+            self.listToSort[j]=temp
+            self.quickSort(left,j-1)
+            self.quickSort(j+1,right)
+
+
+
+
+
+
+
+
+q = QuickSort(l)
+print(q)
+q.quickSort(0,len(l)-1)
+print(q)
+
+
