fbfc94d08346933c94cbb9c2378e7a758036a47a
serajago <kumaran_5555@outlook.com>
2014-11-18 21:04:22 +0530

    added code for quicksort

--- Sorting/QuickSort.py
+++ Sorting/QuickSort.py
@@ -1,4 +1,5 @@
 l = [ 10,12,3,4,56,7,0,8,9,11,12]
+l=[0,1,2,3,4,5,-1]
 class QuickSort():
     def __init__(self, listToSort):
         self.listToSort = listToSort
@@ -36,6 +37,25 @@ class QuickSort():
             self.listToSort[j]=temp
             self.quickSort(left,j-1)
             self.quickSort(j+1,right)
+    def testQuickSort(self,left,right):
+        i=left
+        j=right
+        pivot=self.listToSort[int(left+(right-left)/2)]
+        while(i<=j):
+            while(self.listToSort[i]<pivot):
+                i=i+1
+            while(self.listToSort[i]>pivot):
+                j=j-1
+            if(i<=j):
+                tmp=self.listToSort[i]
+                self.listToSort[j]=self.listToSort[i]
+                self.listToSort[i]=tmp
+                i=i+1
+                j=j-1
+        if(i<right):
+            self.testQuickSort(left,i)
+        if(j>left):
+            self.testQuickSort(j,right)
 
 
 
@@ -45,9 +65,6 @@ class QuickSort():
 
 
 q = QuickSort(l)
-print(q)
-q.quickSort(0,len(l)-1)
-print(q)
-
+q.testQuickSort(0,len(l)-1)
 
 print(QuickSort.quickSort.__doc__)
