5be6a7221d2a366dbd8048f544f1ea0c0baa715f
kumaran-5555 <kumaran_5555@outlook.com>
2014-11-26 11:11:26 +0530

    cosmetics

--- Sorting/QuickSort.py
+++ Sorting/QuickSort.py
@@ -1,8 +1,11 @@
-l = [ 10,12,3,4,56,7,0,8,9,11,12]
-l=[0,1,2,3,4,5,-1]
+l = [10, 12, 3, 4, 56, 7, 0, 8, 9, 11, 12]
+l = [0, 1, 2, 3, 4, 5, -1]
+
+
 class QuickSort():
     def __init__(self, listToSort):
         self.listToSort = listToSort
+
     def __str__(self):
         return str(self.listToSort)
 
@@ -13,58 +16,53 @@ class QuickSort():
         """
         if left < right:
 
-            pivotIdx=int(left+((right-left)/2))
-            pivotNewIdx=pivotIdx
-            i=left
-            j=right
-            pivot=self.listToSort[pivotIdx]
-            while(i<j):
-                while (self.listToSort[i]<=pivot and i<right):
-                    i=i+1
-                while(self.listToSort[j] > pivot):
-                    j=j-1
-                if(i<j):
-                    if(i==pivotIdx):
-                        pivotNewIdx=j
-                    elif(j==pivotIdx):
-                        pivotNewIdx=i
+            pivotIdx = int(left + ((right - left) / 2))
+            pivotNewIdx = pivotIdx
+            i = left
+            j = right
+            pivot = self.listToSort[pivotIdx]
+            while (i < j):
+                while (self.listToSort[i] <= pivot and i < right):
+                    i = i + 1
+                while (self.listToSort[j] > pivot):
+                    j = j - 1
+                if (i < j):
+                    if (i == pivotIdx):
+                        pivotNewIdx = j
+                    elif (j == pivotIdx):
+                        pivotNewIdx = i
 
-                    temp=self.listToSort[i]
-                    self.listToSort[i]=self.listToSort[j]
-                    self.listToSort[j]=temp
-            temp=self.listToSort[pivotNewIdx]
+                    temp = self.listToSort[i]
+                    self.listToSort[i] = self.listToSort[j]
+                    self.listToSort[j] = temp
+            temp = self.listToSort[pivotNewIdx]
             self.listToSort[pivotNewIdx] = self.listToSort[j]
-            self.listToSort[j]=temp
-            self.quickSort(left,j-1)
-            self.quickSort(j+1,right)
-    def testQuickSort(self,left,right):
-        i=left
-        j=right
-        pivot=self.listToSort[int(left+(right-left)/2)]
-        while(i<=j):
-            while(self.listToSort[i]<pivot):
-                i=i+1
-            while(self.listToSort[j]>pivot):
-                j=j-1
-            if(i<=j):
-                tmp=self.listToSort[j]
-                self.listToSort[j]=self.listToSort[i]
-                self.listToSort[i]=tmp
-                i=i+1
-                j=j-1
-        if(i<right):
-            self.testQuickSort(left,i)
-        if(j>left):
-            self.testQuickSort(j,right)
+            self.listToSort[j] = temp
+            self.quickSort(left, j - 1)
+            self.quickSort(j + 1, right)
 
-
-
-
-
-
+    def testQuickSort(self, left, right):
+        i = left
+        j = right
+        pivot = self.listToSort[int(left + (right - left) / 2)]
+        while (i <= j):
+            while (self.listToSort[i] < pivot):
+                i = i + 1
+            while (self.listToSort[j] > pivot):
+                j = j - 1
+            if (i <= j):
+                tmp = self.listToSort[j]
+                self.listToSort[j] = self.listToSort[i]
+                self.listToSort[i] = tmp
+                i = i + 1
+                j = j - 1
+        if (i < right):
+            self.testQuickSort(left, i)
+        if (j > left):
+            self.testQuickSort(j, right)
 
 
 q = QuickSort(l)
-q.testQuickSort(0,len(l)-1)
+q.testQuickSort(0, len(l) - 1)
 print(q.listToSort)
 print(QuickSort.quickSort.__doc__)
--- Sorting/SelectionSort.py
+++ Sorting/SelectionSort.py
@@ -1,8 +1,11 @@
 __author__ = 'serajago'
-l = [ 10,12,3,4,56,7,0,8,9,11,12]
+l = [10, 12, 3, 4, 56, 7, 0, 8, 9, 11, 12]
+
+
 class SelectionSort():
     def __init__(self, listToSort):
         self.listToSort = listToSort
+
     def __str__(self):
         return str(self.listToSort)
 
@@ -11,29 +14,22 @@ class SelectionSort():
 
         :rtype : object
         """
-        for i in range(0,len(self.listToSort)-1):
-            minVal=self.listToSort[i]
-            minIdx=i
-            for j in range(i+1,len(self.listToSort)-1):
+        for i in range(0, len(self.listToSort) - 1):
+            minVal = self.listToSort[i]
+            minIdx = i
+            for j in range(i + 1, len(self.listToSort) - 1):
                 if self.listToSort[j] < minVal:
-                    minIdx=j
-                    minVal=self.listToSort[j]
-            temp=self.listToSort[i]
-            self.listToSort[i]=self.listToSort[minIdx]
-            self.listToSort[minIdx]=temp
-
-
-
-
-
-
-
+                    minIdx = j
+                    minVal = self.listToSort[j]
+            temp = self.listToSort[i]
+            self.listToSort[i] = self.listToSort[minIdx]
+            self.listToSort[minIdx] = temp
 
 
 q = SelectionSort(l)
 print(q)
-q.selectionSort(0,len(l)-1)
+q.selectionSort(0, len(l) - 1)
 print(q)
 
 
-#print(SelectionSort.quickSort.__doc__)
+# print(SelectionSort.quickSort.__doc__)
